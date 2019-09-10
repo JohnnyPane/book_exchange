6a2b7c3903ea4af08d63e25738760bb91214a647
JohnnyP <wabanichi@gmail.com>
2019-09-10 13:26:36 -0500

    set up store

--- frontend/book_exchange.jsx
+++ frontend/book_exchange.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { login, signup } from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-
-  window.login = login;
-  window.signup = signup;
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch= store.dispatch;
   ReactDOM.render(<h1>Welcome to The Book Exchange</h1>, root);
 });
