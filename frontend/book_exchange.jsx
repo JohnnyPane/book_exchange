be3143f22417f00eb53ae69f389979fb5ae0853d
JohnnyP <wabanichi@gmail.com>
2019-09-11 16:27:20 -0500

    bootstrap current user

--- frontend/book_exchange.jsx
+++ frontend/book_exchange.jsx
@@ -6,10 +6,19 @@ import { login, signup } from './util/session_api_util';
 import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      session: { id: window.currentUser.id },
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser } 
+      }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   const root = document.getElementById("root");
-  const store = configureStore();
-  window.login = login();
-  window.getState = store.getState;
-  window.dispatch= store.dispatch;
   ReactDOM.render(<Root store={store} />, root);
 });
