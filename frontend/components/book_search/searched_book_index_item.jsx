377c2712ce783383d0148f4a9fe4c904a4e1a1b9
JohnnyP <wabanichi@gmail.com>
2019-09-19 20:03:34 -0500

    add book to wishlist search

--- frontend/components/book_search/searched_book_index_item.jsx
+++ frontend/components/book_search/searched_book_index_item.jsx
@@ -3,11 +3,19 @@ import React from 'react';
 class SearchedBookIndexItem extends React.Component {
   constructor(props) {
     super(props);
-    this.handleSubmit =this.handleSubmit.bind(this);
+    this.state = {
+      wishlist_id: ""
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.update = this.update.bind(this);
   }
 
   componentDidMount() {
     console.log(this.props, "search index item");
+  }
+
+  update(property) {
+    return e => this.setState({ [property]: e.currentTarget.value });
   }
 
   // NEED TO CHANGE WISHLIST!!!!
@@ -18,7 +26,7 @@ class SearchedBookIndexItem extends React.Component {
       authors: this.props.book.volumeInfo.authors.join(", "),
       imageURL: this.props.book.volumeInfo.imageLinks.smallThumbnail,
       description: this.props.book.volumeInfo.description,
-      wishlist_id: 1
+      wishlist_id: this.state.wishlist_id
     };
     console.log(newBook);
     this.props.createBook(newBook);
@@ -39,17 +47,17 @@ class SearchedBookIndexItem extends React.Component {
           <p>{volumeInfo.description}</p>
           {volumeInfo.hasOwnProperty('imageLinks') ? renderImage() : null}
         </div>
-        {/* <button className="create-book-btn" onClick={this.handleSubmit}>Add to Wish List</button> */}
         <div className="dropdown">
           <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown
+            Add to a wishlist
           </button>
           <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
             {wishlists.map((list, i) => {
-              return <button className="dropdown-item" type="button" key={i} onClick={this.handleSubmit}>{list.title}</button>
+              return <button className="dropdown-item" type="button" key={i} value={list.id} onClick={this.update('wishlist_id')}>{list.title}</button>
             })}
           </div>
         </div>
+        <button className="create-book-btn" onClick={this.handleSubmit}>Add to Wish List</button>
       </div>
     )
   }
